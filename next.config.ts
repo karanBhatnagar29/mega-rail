dc1a448e29f0d147d0302b318e0b8bce82b53953
karanBhatnagar29 <karanbhatnagar862@gmail.com>
2025-09-18 23:36:21 +0530

    root set

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com"], // 👈 allow Cloudinary images
   },
+  turbopack: {
+    root: __dirname, // 👈 explicitly set your project root
+  },
 };
 
 export default nextConfig;
