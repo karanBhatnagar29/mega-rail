7545f5a2bbe979e98041862c87327d31e2de3ee0
karanBhatnagar29 <karanbhatnagar862@gmail.com>
2025-12-26 10:02:45 +0530

    solved qr issue

--- components/ui/cardList.tsx
+++ components/ui/cardList.tsx
@@ -43,7 +43,7 @@ export default function Card({
 }: Props) {
   const [flipped, setFlipped] = useState(false);
 
-  const cardUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/card/view/${card._id}`;
+  const cardUrl = `https://megarailpowerproject.vercel.app/card/view/${card._id}`;
 
   const ORANGE_COLOR = "#ff6a00";
   const fallbackImage = "/mnt/data/dcc878a1-b711-4227-a41e-c9020e9bdae9.png";
